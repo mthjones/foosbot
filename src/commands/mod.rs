3199d0130e7d96cee092192b51adf5c0702b1b54
Matt Jones <mthjones@gmail.com>
2015-10-02 17:07:25 -0400

    Add 1v1 game style

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -83,16 +83,17 @@ impl GameCommandHandler {
 }
 
 impl CommandHandler for GameCommandHandler {
-    fn handle(&mut self, sender: &mut Sender, _: &Vec<String>) {
+    fn handle(&mut self, sender: &mut Sender, args: &Vec<String>) {
+        let team_size = if args.len() > 0 && args[0] == "1v1" { 1 } else { 2 };
         let users = self.registered_users.borrow();
-        if users.len() < 4 {
-            sender.channel.write("Not enough players! Need 4 players to be registered to start a game.").unwrap();
+        if users.len() < team_size * 2 {
+            sender.channel.write(format!("Not enough players! Need {0} players to be registered to start a game.", team_size * 2)).unwrap();
         } else {
             let mut users = &mut users.iter().collect::<Vec<_>>()[..];
             let mut rng = rand::thread_rng();
             rng.shuffle(users);
-            let team1 = &users[..2];
-            let team2 = &users[2..4];
+            let team1 = &users[..team_size];
+            let team2 = &users[team_size..team_size * 2];
             sender.channel.write(format!("⚽ We have a game! {:?} vs {:?}", team1, team2)).unwrap();
         }
     }
